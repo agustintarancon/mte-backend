5e11a8259ea13636b8aadfcd28febac80f8c1442
agustintarancon <agustinbuf@gmail.com>
2023-11-30 11:30:27 -0300

    salario, faltas y advertencias

--- models/user.model.js
+++ models/user.model.js
@@ -22,6 +22,18 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  salary: {
+    type: Number,
+    default: 0
+  },
+  faults: {
+    type: Number,
+    default: 0
+  },
+  warnings: {
+    type: Number,
+    default: 0
+  },
 });
 
 module.exports = model("users", userSchema);
