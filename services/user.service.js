34998876f5979ee22e9602db5d1d2773715b3d26
agustintarancon <agustinbuf@gmail.com>
2024-03-12 14:21:50 -0300

    fix

--- services/user.service.js
+++ services/user.service.js
@@ -1,7 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
-const registerUserService = async ({userName, document, password, category}) => {
 const registerUserService = async ({userName,document,password,category,}) => {
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
